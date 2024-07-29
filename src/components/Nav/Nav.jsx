c07fec97842db4ca996f1ecdcac40a86964f8dd3
Christophe <christophegauthiersokpoly@gmail.com>
2024-07-29 15:24:38 +0200

    correction

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,36 @@
+import { useRef } from 'react';
 import './Nav.css';
+import { FaBars, FaTimes } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
+
+  const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
   return (
     <div className='top_section'>
       <span>ROBERT GARCIA</span>
-      <div className='nav_links'>
-        <a href="#"><NavLink exact to="/">Work</NavLink></a>
-        <a href="#"><NavLink to="/about_page">About</NavLink></a>
-        <a href="#"><NavLink to="/contact">Contact</NavLink></a>
+      <div className='nav_links' id="nav-bar" ref={navRef}>
+        <NavLink exact to="/">Work</NavLink>
+        <NavLink to="/about_page">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
       </div>
-      <a href="" className='berguer'><i class="fa-solid fa-bars-staggered fa-2xl" style={{color: "#c7c7c7"}}></i></a>
+      <button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
     </div>
   )
 }
